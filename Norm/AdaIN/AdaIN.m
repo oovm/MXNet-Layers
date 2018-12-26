0f5a9209a3f48c42dc7f53a21b788ee33a7e7817
GalAster <galaster@foxmail.com>
2018-12-26 11:51:15 +0800

    更新::Fix shape interface broken of AdaIN

--- Norm/AdaIN/AdaIN.m
+++ Norm/AdaIN/AdaIN.m
@@ -1,16 +1,21 @@
 Inputs:
-	$Content: ChannelT[$$Channels, TensorT[$$Dimensions]]
-	$Style: ChannelT[$$Channels, TensorT[$$Dimensions]]
+	$Content: ChannelT[$$Channels, TensorT[$$ContentSize]]
+	$Style: ChannelT[$$Channels, TensorT[$$StyleSize]]
 
-Output: ChannelT[$$Channels, TensorT[$$Dimensions]]
+
+Outputs:
+	$Output: ChannelT[$$Channels, TensorT[$$ContentSize]]
 
 Parameters:
 	$Epsilon: Defaulting[ScalarT, 10^-5]
+	$$Channels: SizeT
+	$$ContentSize: SizeListT[2]
+	$$StyleSize: SizeListT[2]
 
 
 AllowDynamicDimensions: True
 
-
+(*
 SowMeanSigma[input_, eps_] := Scope[
 	mean = SowNode["mean", input, "axis" -> {2, 3}, "keepdims" -> True];
 	var = SowSquare@SowBMinus[input, mean];
@@ -18,17 +23,26 @@ SowMeanSigma[input_, eps_] := Scope[
 	sigma = SowSqrt@SowBPlus[var, eps];
 	Return@{mean, sigma}
 ]
+*)
 
+GetMean[input_] := SowNode["mean", input, "axis" -> {2, 3}, "keepdims" -> True];
+GetSigma[input_, mean_, eps_] := Scope[
+	sub = SowSquare@SowBMinus[input, mean];
+	var = SowNode["mean", sub, "axis" -> {2, 3}, "keepdims" -> True];
+	SowSqrt@SowPlusScalar[var, eps]
+];
 
 Writer: Function[
 	ctx = GetInput["Content", "Batchwise"];
 	sty = GetInput["Style", "Batchwise"];
-	{sMean, sSigma} = SowMeanSigma[sty, #Epsilon];
-	{cMean, cSigma} = SowMeanSigma[ctx, #Epsilon];
+	{sMean, cMean} = GetMean /@ {sty, ctx};
+	sSigma = GetSigma[sty, sMean, #Epsilon];
+	cSigma = GetSigma[ctx, cMean, #Epsilon];
 	sub = SowBMinus[ctx, cMean];
 	div = SowBDivide[sSigma, cSigma];
 	out = SowBPlus[SowBHad[sub, div], sMean];
 	SetOutput["Output", out];
 ]
+
 
 Suffix: "Layer"
